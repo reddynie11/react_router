168a3848fe382eb90087a63fd017d60076c780a4
Narasimha Reddy <macbook@APPLEs-MacBook-Air.local>
2019-07-22 21:21:08 +0530

    Routes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+//components
+import Home from './components/home';
+import Post from './components/post';
+import Profile from './components/profile';
+
+const App = ()=>{
+    return(
+        <BrowserRouter>
+            <div>
+                <header>NavBar</header>
+                <Route path='/' exact component={Home} />
+                <Route path='/posts' component={Post} />
+                <Route path='/profile' component={Profile} />
+            </div>
+            
+        </BrowserRouter>
+    )
+}
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
